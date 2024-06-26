cf6f332b101ea0e84ef314503aa8ec831d41421e
majesticrock <joshua.althueser@googlemail.com>
2024-06-26 11:15:54 +0200

    Interpolation

--- Utility/include/Utility/Numerics/Interpolation.hpp
+++ Utility/include/Utility/Numerics/Interpolation.hpp
@@ -5,6 +5,11 @@
 #include <type_traits>
 
 namespace Utility::Numerics {
+	namespace __detail {
+		template<class _container>
+		using element_t = std::decay_t<decltype(std::declval<_container>()[0])>;
+	}
+
 	template <class x_type, class y_type>
 	constexpr y_type linearly_interpolate(x_type const& x, x_type const& x0, x_type const& x1, y_type const& y0, y_type const& y1) 
 	{
@@ -29,23 +34,20 @@ namespace Utility::Numerics {
 	}
 
 	template <unsigned int n, class x_vector, class y_vector>
-	inline std::decay_t<decltype(std::declval<y_vector>()[0])> interpolate_from_vector(
-		std::decay_t<decltype(std::declval<x_vector>()[0])> const& x, 
-		x_vector const& x_data, y_vector const& y_data, unsigned int start_index_x, int index_offset_y = 0U)
+	inline __detail::element_t<y_vector> interpolate_from_vector(__detail::element_t<x_vector> const& x, 
+		x_vector const& x_data, y_vector const& y_data, unsigned int start_index_x, int index_offset_y = 0)
 	{
-		using x_type = std::decay_t<decltype(std::declval<x_vector>()[0])>;
-		using y_type = std::decay_t<decltype(std::declval<y_vector>()[0])>;
 		assert(x_data.size() >= n && y_data.size() >= n + index_offset_y);
 
 		if(start_index_x + n >= x_data.size()) {
 			start_index_x = x_data.size() - n;
 		}
 
-		std::array<x_type, n> x_arr;
-		std::array<y_type, n> y_arr;
+		std::array<__detail::element_t<x_vector>, n> x_arr;
+		std::array<__detail::element_t<y_vector>, n> y_arr;
 		for(unsigned int i = 0U; i < n; ++i) {
-			x_arr[i] = x_data[i];
-			y_arr[i] = y_data[i + index_offset_y];
+			x_arr[i] = x_data[start_index_x + i];
+			y_arr[i] = y_data[start_index_x + i + index_offset_y];
 		}
 
 		return interpolate_lagrange<n>(x, x_arr, y_arr);
