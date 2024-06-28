990ddce652d19787c36522da30353aa678470d21
majesticrock <joshua.althueser@googlemail.com>
2024-06-28 14:32:19 +0200

    Added cascadelake march
    Added cascadelake march
    
    Added cascadelake march
    
    Added cascadelake march
    
    Added cascadelake march
    
    Added cascadelake march
    
    Added cascadelake march
    
    Added cascadelake march
    
    Added cascadelake march

---
+++ SymbolicOperators/cmake/DefaultCompilerFlags.cmake
@@ -0,0 +1,13 @@
+# DefaultCompilerFlags.cmake
+
+function(SET_COMPILER_FLAGS TARGET)
+    if(CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+        if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS 12.1)
+            message(FATAL_ERROR "GCC version ${CMAKE_CXX_COMPILER_VERSION} is not supported. GCC 12.1 or newer is required for C++20 support.")
+        endif()
+
+        target_compile_options(${TARGET} PRIVATE -Wall -Wno-sign-compare -fopenmp -march=native -O3)
+    else()
+        message(FATAL_ERROR "Unsupported compiler ${CMAKE_CXX_COMPILER_ID}. Only GCC is supported.")
+    endif()
+endfunction()
