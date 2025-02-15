c023c8246c7a02ceb486f7f6326fc4c80af24edb
Joshua <joshua.althueser@gmail.com>
2025-02-15 12:14:07 +0100

    Functiontime for void

--- utility/include/mrock/utility/FunctionTime.hpp
+++ utility/include/mrock/utility/FunctionTime.hpp
@@ -9,42 +9,40 @@ namespace mrock::utility {
 	using clock = std::chrono::high_resolution_clock;
 
 	// Prints the runtime of a function to the console and returns the function's return value
-	template <class FunctionTypes, class... Args>
-	auto function_time_ms(const FunctionTypes& func, Args&&... args)
+	template <class FunctionType, class... Args>
+	auto function_time_ms(const FunctionType& func, Args&&... args)
 		-> decltype(func(std::forward<Args>(args)...))
 	{
 		clock::time_point begin = clock::now();
-		if constexpr (!(std::is_same_v<decltype(func(std::forward<Args>(args)...)), void>)) {
+		if constexpr (!(std::is_void_v<decltype(func(std::forward<Args>(args)...))>)) {
 			auto return_value = func(std::forward<Args>(args)...);
 			clock::time_point end = clock::now();
 			std::cout << "Runtime = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
 			return return_value;
 		}
 		else {
-			auto return_value = func(std::forward<Args>(args)...);
+			func(std::forward<Args>(args)...);
 			clock::time_point end = clock::now();
 			std::cout << "Runtime = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
-			return return_value;
 		}
 	}
 
 	// Prints the runtime of a function to the console and returns the function's return value
-	template <class FunctionTypes, class... Args>
-	auto function_time_micro(const FunctionTypes& func, Args&&... args)
+	template <class FunctionType, class... Args>
+	auto function_time_micro(const FunctionType& func, Args&&... args)
 		-> decltype(func(std::forward<Args>(args)...))
 	{
 		clock::time_point begin = clock::now();
-		if constexpr (!(std::is_same_v<decltype(func(std::forward<Args>(args)...)), void>)) {
+		if constexpr (!(std::is_void_v<decltype(func(std::forward<Args>(args)...))>)) {
 			auto return_value = func(std::forward<Args>(args)...);
 			clock::time_point end = clock::now();
 			std::cout << "Runtime = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[microseconds]" << std::endl;
 			return return_value;
 		}
 		else {
-			auto return_value = func(std::forward<Args>(args)...);
+			func(std::forward<Args>(args)...);
 			clock::time_point end = clock::now();
 			std::cout << "Runtime = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[microseconds]" << std::endl;
-			return return_value;
 		}
 	}
 
@@ -53,17 +51,16 @@ namespace mrock::utility {
 	ReturnType member_function_time_ms(ObjectType& obj, ReturnType(ObjectType::* function)(Args...), Args2&&... args)
 	{
 		clock::time_point begin = clock::now();
-		if constexpr (!std::is_same_v<decltype((obj.*function)(std::forward<Args2>(args)...)), void>) {
+		if constexpr (!std::is_void_v<decltype((obj.*function)(std::forward<Args2>(args)...))>) {
 			auto return_value = (obj.*function)(std::forward<Args2>(args)...);
 			clock::time_point end = clock::now();
 			std::cout << "Runtime = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
 			return return_value;
 		}
 		else {
-			auto return_value = (obj.*function)(std::forward<Args2>(args)...);
+			(obj.*function)(std::forward<Args2>(args)...);
 			clock::time_point end = clock::now();
 			std::cout << "Runtime = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
-			return return_value;
 		}
 	}
 
@@ -72,17 +69,16 @@ namespace mrock::utility {
 	auto member_function_time_micro(ObjectType& obj, ReturnType(ObjectType::* function)(Args...), Args2&&... args)
 	{
 		clock::time_point begin = clock::now();
-		if constexpr (!std::is_same_v<decltype((obj.*function)(std::forward<Args2>(args)...)), void>) {
+		if constexpr (!std::is_void_v<decltype((obj.*function)(std::forward<Args2>(args)...))>) {
 			auto return_value = (obj.*function)(std::forward<Args2>(args)...);
 			clock::time_point end = clock::now();
 			std::cout << "Runtime = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[microseconds]" << std::endl;
 			return return_value;
 		}
 		else {
-			auto return_value = (obj.*function)(std::forward<Args2>(args)...);
+			(obj.*function)(std::forward<Args2>(args)...);
 			clock::time_point end = clock::now();
 			std::cout << "Runtime = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[microseconds]" << std::endl;
-			return return_value;
 		}
 	}
 }
