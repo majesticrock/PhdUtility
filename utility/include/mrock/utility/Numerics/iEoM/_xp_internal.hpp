f694454143d75b377172d7a264846cba8bb629d8
majesticrock <joshua.althueser@googlemail.com>
2026-01-20 18:55:29 +0100

    Refactor XPResolvent

--- utility/include/mrock/utility/Numerics/iEoM/_xp_internal.hpp
+++ utility/include/mrock/utility/Numerics/iEoM/_xp_internal.hpp
@@ -3,8 +3,19 @@
 #include <string>
 #include <numeric>
 #include <algorithm>
+#include <type_traits>
 
 namespace mrock::utility::Numerics::iEoM { 
+    struct state_iterator_tag {};
+
+    template<typename It>
+    concept StateIterator = requires { 
+            typename It::state_iterator_tag; 
+        } && std::same_as<typename It::state_iterator_tag, mrock::utility::Numerics::iEoM::state_iterator_tag>;
+
+    template<typename It>
+    concept ConstStateIterator = StateIterator<It> && It::is_const_iterator;
+
     template<class RealType>
 	struct StartingState {
 		using Vector = Eigen::Vector<RealType, Eigen::Dynamic>;
@@ -52,91 +63,82 @@ namespace mrock::utility::Numerics::iEoM {
             });
     }
 
-    template<class RealType>
-    class PhaseIterator {
+    template<class RealType, bool iteratesPhase, bool isConst>
+    class StateIteratorImpl {
+        using container_type = std::conditional_t<isConst,
+            const std::vector<StartingState<RealType>>,
+            std::vector<StartingState<RealType>>>;
+        
+        using value_type_impl = std::conditional_t<isConst,
+            const StartingState<RealType>,
+            StartingState<RealType>>;
     public:
         using iterator_category = std::forward_iterator_tag;
-        using value_type = StartingState<RealType>;
-        using difference_type = std::ptrdiff_t;
-        using pointer = value_type*;
-        using reference = value_type&;
+        using value_type        = StartingState<RealType>;
+        using difference_type   = std::ptrdiff_t;
+        using pointer           = value_type_impl*;
+        using reference         = value_type_impl&;
 
-        PhaseIterator(std::vector<StartingState<RealType>>& vec, size_t pos = 0U)
+        using Vector = typename StartingState<RealType>::Vector;
+        static constexpr bool is_const_iterator = isConst;
+        using state_iterator_tag = mrock::utility::Numerics::iEoM::state_iterator_tag;
+
+        StateIteratorImpl(container_type& vec, size_t pos = 0U)
             : m_vec(vec), m_pos(pos) { skip_invalid(); }
-        static PhaseIterator begin(std::vector<StartingState<RealType>>& vec) {
-            return PhaseIterator<RealType>(vec, 0U);
+        static StateIteratorImpl begin(container_type& vec) {
+            return StateIteratorImpl(vec, 0U);
         }
-        static PhaseIterator end(std::vector<StartingState<RealType>>& vec) {
-            return PhaseIterator<RealType>(vec, vec.size());
+        static StateIteratorImpl end(container_type& vec) {
+            return StateIteratorImpl(vec, vec.size());
         }
 
-        PhaseIterator& operator++() {
+        StateIteratorImpl& operator++() {
             ++m_pos;
             skip_invalid();
             return *this;
         }
-        PhaseIterator operator++(int) {
-            PhaseIterator tmp = *this;
+        StateIteratorImpl operator++(int) {
+            auto tmp = *this;
             ++(*this);
             return tmp;
         }
 
-        bool operator==(const PhaseIterator& other) const { return m_pos == other.m_pos; }
-        bool operator!=(const PhaseIterator& other) const { return !(*this == other); }
+        bool operator==(const StateIteratorImpl& other) const { return m_pos == other.m_pos; }
+        bool operator!=(const StateIteratorImpl& other) const { return !(*this == other); }
         reference operator*() { return m_vec[m_pos]; }
         pointer operator->() { return &m_vec[m_pos]; }
 
+        auto& state() const 
+        { 
+            if constexpr (iteratesPhase)
+                return m_vec[m_pos].phase_state; 
+            else 
+                return m_vec[m_pos].amplitude_state;
+        }
     private:
-        std::vector<StartingState<RealType>>& m_vec;
+        container_type& m_vec;
         size_t m_pos;
         void skip_invalid() {
-            while (m_pos < m_vec.size() && !m_vec[m_pos].contains_phase_state()) {
-                ++m_pos;
+            if constexpr (iteratesPhase) {
+                while (m_pos < m_vec.size() && !m_vec[m_pos].contains_phase_state()) {
+                    ++m_pos;
+                }
+            }
+            else {
+                while (m_pos < m_vec.size() && !m_vec[m_pos].contains_amplitude_state()) {
+                    ++m_pos;
+                }
             }
         }
     };
 
     template<class RealType>
-    class AmplitudeIterator {
-    public:
-        using iterator_category = std::forward_iterator_tag;
-        using value_type = StartingState<RealType>;
-        using difference_type = std::ptrdiff_t;
-        using pointer = value_type*;
-        using reference = value_type&;
+    using PhaseIterator = StateIteratorImpl<RealType, true, false>;
+    template<class RealType>
+    using ConstPhaseIterator = StateIteratorImpl<RealType, true, true>;
 
-        AmplitudeIterator(std::vector<StartingState<RealType>>& vec, size_t pos = 0U)
-            : m_vec(vec), m_pos(pos) { skip_invalid(); }
-        static AmplitudeIterator begin(std::vector<StartingState<RealType>>& vec) {
-            return AmplitudeIterator<RealType>(vec, 0U);
-        }
-        static AmplitudeIterator end(std::vector<StartingState<RealType>>& vec) {
-            return AmplitudeIterator<RealType>(vec, vec.size());
-        }
-
-        AmplitudeIterator& operator++() {
-            ++m_pos;
-            skip_invalid();
-            return *this;
-        }
-        AmplitudeIterator operator++(int) {
-            AmplitudeIterator tmp = *this;
-            ++(*this);
-            return tmp;
-        }
-
-        bool operator==(const AmplitudeIterator& other) const { return m_pos == other.m_pos; }
-        bool operator!=(const AmplitudeIterator& other) const { return !(*this == other); }
-        reference operator*() { return m_vec[m_pos]; }
-        pointer operator->() { return &m_vec[m_pos]; }
-
-    private:
-        std::vector<StartingState<RealType>>& m_vec;
-        size_t m_pos;
-        void skip_invalid() {
-            while (m_pos < m_vec.size() && !m_vec[m_pos].contains_amplitude_state()) {
-                ++m_pos;
-            }
-        }
-    };
+    template<class RealType>
+    using AmplitudeIterator = StateIteratorImpl<RealType, false, false>;
+    template<class RealType>
+    using ConstAmplitudeIterator = StateIteratorImpl<RealType, false, true>;
 }
