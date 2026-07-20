c4645e2dc4666012198867a13ca3138453f5af10
majesticrock <joshua.althueser@googlemail.com>
2026-07-20 16:13:16 +0200

    update get_data

--- python/mrock/get_data.py
+++ python/mrock/get_data.py
@@ -4,123 +4,135 @@ import json
 import pandas as pd
 import glob
 import os
-CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+from pathlib import Path
 
-def __to_path(model, subfolder, **kwargs):
-    if len(kwargs) != 0:
-        params = os.path.join(*(f"{key}={value}" for key, value in kwargs.items()))
-        return os.path.join(CURRENT_DIR, model, os.path.normpath(subfolder), params)
-    return os.path.join(CURRENT_DIR, model, os.path.normpath(subfolder))
+CURRENT_DIR = Path(__file__).resolve().parent
+DEFAULT_DATA_DIR = CURRENT_DIR.parents[2] / "data"
 
-def convert_to_numpy(obj):
+def _float_or_str(obj):
+    return (float(obj) if not isinstance(obj, str) else obj)
+
+def _convert_to_numpy(obj):
     if isinstance(obj, list):
         return np.array(obj)
     if isinstance(obj, dict):
-        return {k: convert_to_numpy(v) for k, v in obj.items()}
+        return {k: _convert_to_numpy(v) for k, v in obj.items()}
     return obj
 
-def load_panda_file(file, numpy_conversion=True):
-    with gzip.open(file, 'rt') as f_open:
-        if numpy_conversion:
-            jData = json.load(f_open, object_hook=convert_to_numpy)
+class DataLoader:
+    def __init__(self, data_dir=None):
+        if data_dir is None:
+            self.data_dir = os.environ.get("MROCK_DATA_DIR", DEFAULT_DATA_DIR)
+        else:    
+            self.data_dir = Path(data_dir).expanduser().resolve()
+
+    def _to_path(self, model, subdir, **kwargs):
+        base = self.data_dir / model / Path(subdir)
+
+        if kwargs:
+            params = Path(*[f"{key}={value}" for key, value in kwargs.items()])
+            return base / params
+
+        return base
+
+    def load_panda_file(self, file, numpy_conversion=True):
+        with gzip.open(file, "rt") as f_open:
+            if numpy_conversion:
+                jData = json.load(f_open, object_hook=_convert_to_numpy)
+            else:
+                jData = json.load(f_open)
+
+        if "data" in jData:
+            main_data = {k: v for k, v in jData.items() if k != "data"}
+            main_df = pd.Series(main_data)
+            main_df["data"] = pd.DataFrame(jData["data"])
         else:
-            jData = json.load(f_open)
-    
-    if 'data' in jData:
-        main_data = {k: v for k, v in jData.items() if k != 'data'}
-        main_df = pd.Series(main_data)
-        main_df['data'] = pd.DataFrame(jData['data'])
-    else:
-        main_df = pd.json_normalize(jData, max_level=1).iloc[0]
-    return main_df
+            main_df = pd.json_normalize(jData, max_level=1).iloc[0]
 
-def load_panda(model, subfolder, file, print_date=True, numpy_conversion=True, **kwargs):
-    data = load_panda_file(os.path.join(__to_path(model, subfolder, **kwargs), file), numpy_conversion=numpy_conversion)
-    if print_date:
-        print(f"Loaded data has been produced on {data['time']}")
-    return data
+        return main_df
 
-def __float_or_str__(obj):
-    return (float(obj) if not isinstance(obj, str) else obj)
+    def load_panda(self, model, subdir, file, print_date=True, numpy_conversion=True, **kwargs):
+        path = self._to_path(model, subdir, **kwargs) / file
+        data = self.load_panda_file(path, numpy_conversion=numpy_conversion)
+
+        if print_date:
+            print(f"Loaded data has been produced on {data['time']}")
+
+        return data
+
+    def get_all_files(self, folder, pattern):
+        folder = Path(folder)
+        search_obj = self.data_dir / folder / "**" / pattern
+        return glob.glob(str(search_obj), recursive=True)
+
+    def load_all(self, folder, pattern, condition=None):
+        all_files = self.get_all_files(folder, pattern)
+        if condition is not None:
+            if not isinstance(condition, str):
+                for cond in condition:
+                    if os.name == 'nt':
+                        cond = f"\\{cond}\\"
+                    else:
+                        cond = f"/{cond}/"
+                    all_files = [file for file in all_files if cond in file]
+            else:
+                if os.name == 'nt':
+                    cond = f"\\{condition}\\"
+                else:
+                    cond = f"/{condition}/"
+                all_files = [file for file in all_files if cond in file]
+        return pd.concat([self.load_panda_file(file) for file in all_files], ignore_index=True, axis=1).T.convert_dtypes()
+
+    def load_pickle(self, model, subdir, file):
+        path = self._to_path(model, subdir) / file
+        return pd.read_pickle(path)
 
 def continuum_params(N_k, T, coulomb_scaling, screening, k_F, g, omega_D):
     cp_screening = 0.0 if abs(coulomb_scaling) < 1e-12 else screening
     return  {
                 "N_k"             : int(N_k) if not isinstance(N_k, str) else N_k, 
-                "T"               : __float_or_str__(T),
-                "coulomb_scaling" : __float_or_str__(coulomb_scaling),
-                "screening"       : __float_or_str__(cp_screening),
-                "k_F"             : __float_or_str__(k_F),
-                "g"               : __float_or_str__(g), 
-                "omega_D"         : __float_or_str__(omega_D)
+                "T"               : _float_or_str(T),
+                "coulomb_scaling" : _float_or_str(coulomb_scaling),
+                "screening"       : _float_or_str(cp_screening),
+                "k_F"             : _float_or_str(k_F),
+                "g"               : _float_or_str(g), 
+                "omega_D"         : _float_or_str(omega_D)
             }
 
 def hubbard_params(T, U, V):
     return  {
-                "T": __float_or_str__(T), 
-                "U": __float_or_str__(U), 
-                "V": __float_or_str__(V)
+                "T": _float_or_str(T), 
+                "U": _float_or_str(U), 
+                "V": _float_or_str(V)
             }
 
 def hhg_params(T, E_F, v_F, band_width, field_amplitude, photon_energy, tau_diag, tau_offdiag, t0):
     return  {
-                "T": __float_or_str__(T),
-                "E_F": __float_or_str__(E_F),
-                "v_F": __float_or_str__(v_F),
-                "band_width": __float_or_str__(band_width),
-                "field_amplitude": __float_or_str__(field_amplitude),
-                "photon_energy": __float_or_str__(photon_energy),
-                "tau_diag": __float_or_str__(tau_diag),
-                "tau_offdiag" : __float_or_str__(tau_offdiag),
-                "t0" : __float_or_str__(t0)
+                "T": _float_or_str(T),
+                "E_F": _float_or_str(E_F),
+                "v_F": _float_or_str(v_F),
+                "band_width": _float_or_str(band_width),
+                "field_amplitude": _float_or_str(field_amplitude),
+                "photon_energy": _float_or_str(photon_energy),
+                "tau_diag": _float_or_str(tau_diag),
+                "tau_offdiag" : _float_or_str(tau_offdiag),
+                "t0" : _float_or_str(t0)
             }
 
 def lattice_cut_params(N, g, U, E_F, omega_D):
     return  {
                 "N" : N,
-                "g" : __float_or_str__(g),
-                "U" : __float_or_str__(U),
-                "E_F" : __float_or_str__(E_F),
-                "omega_D" : __float_or_str__ (omega_D)
+                "g" : _float_or_str(g),
+                "U" : _float_or_str(U),
+                "E_F" : _float_or_str(E_F),
+                "omega_D" : _float_or_str (omega_D)
             }
 
 def dwave_params(N, g, V, E_F, omega_D):
     return  {
                 "N" : N,
-                "V" : __float_or_str__(V),
-                "E_F" : __float_or_str__(E_F),
-                "g" : __float_or_str__(g),
-                "omega_D" : __float_or_str__ (omega_D)
+                "V" : _float_or_str(V),
+                "E_F" : _float_or_str(E_F),
+                "g" : _float_or_str(g),
+                "omega_D" : _float_or_str (omega_D)
             }
-
-def get_all_files(folder, pattern):
-    if isinstance(folder, str):
-        folder = os.path.normpath(folder)
-    search_obj = os.path.join(CURRENT_DIR, folder, os.path.normpath("**/"), pattern)
-    return glob.glob(search_obj, recursive=True)
-
-def load_all(folder, pattern, condition=None):
-    all_files = get_all_files(folder, pattern)
-    if condition is not None:
-        if not isinstance(condition, str):
-            for cond in condition:
-                if os.name == 'nt':
-                    cond = f"\\{cond}\\"
-                else:
-                    cond = f"/{cond}/"
-                all_files = [file for file in all_files if cond in file]
-        else:
-            if os.name == 'nt':
-                cond = f"\\{condition}\\"
-            else:
-                cond = f"/{condition}/"
-            all_files = [file for file in all_files if cond in file]
-    return pd.concat([load_panda_file(file) for file in all_files], ignore_index=True, axis=1).T.convert_dtypes()
-
-def load_pickle(folder, file):
-    if isinstance(folder, str):
-        folder = os.path.normpath(folder)
-    return pd.read_pickle(os.path.join(CURRENT_DIR, folder, file))
-
-#pd_data = load_all("continuum/disc_2000", "gap.json.gz")
-#print(pd_data["data"][0]["Delta_Coulomb"] + pd_data["data"][0]["Delta_Phonon"])
